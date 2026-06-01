c54d2bf4d2b5612d67b8f66f3a9efd2a69a87649
Talel Zighni <talelzigni24@gmail.com>
2026-06-01 19:03:59 +0100

    imporved disk analyser

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-nbr_line=0
+# Use 'tail -n +2' to start outputting from the 2nd line, instantly solving the header problem!
+while read -r fs size used avail use mnt; do
+    
+    disk_usage=${use%\%}
+    
+    if (( disk_usage >= 80 )); then
+        echo -e "\e[33m[WARNING] Partition $mnt is at $disk_usage% capacity!\e[0m"
+    fi
 
-while read -r line 
-do
-    if [[ "$nbr_line" == 0 ]];then
-	nbr_line=1
-	continue
-    fi
-    disk_usage=$(echo "$line" | awk '{print $5}')
-    disk_usage=${disk_usage%"%"}
-    mnt_point=$(echo "$line" | awk '{print $6}')
-    if (( disk_usage >= 80 )) ; then
-	echo -e "\e[33m[WARNING] Partition $mnt_point is at $disk_usage% capacity!\e[0m"
-    fi
-done < <(df -h)
+done < <(df -h | tail -n +2)
